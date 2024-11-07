68d3f552af959f2ebfa3f2d1110712a887503340
Eliardo <eliardosilva81@gmail.com>
2024-11-06 22:39:43 -0300

    feat: adicionando navegacao em bottom tabs na tela inicial para navegar para a tela de favoritos

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
 
 import LoginScreen from './views/Login';
 import RegisterScreen from './views/Register';
 import Home from './views/Home';
+import Favorite from './views/Favorite';
 
 export type StackParamList = {
   Login: undefined;
@@ -14,14 +17,46 @@ export type StackParamList = {
 };
 
 const Stack = createStackNavigator<StackParamList>();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => (
+  <Tab.Navigator
+  screenOptions={{
+    headerShown: false,  
+    tabBarActiveTintColor: '#0F90D9',
+    tabBarInactiveTintColor: 'gray',
+  }}
+>
+  <Tab.Screen 
+    name="Home" 
+    component={Home}
+    options={{
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ color, size }) => (
+        <Icon name="home" size={size} color={color} />
+      ),
+    }}
+  />
+  <Tab.Screen 
+    name="Profile" 
+    component={Favorite}
+    options={{
+      tabBarLabel: 'Favoritos',
+      tabBarIcon: ({ color, size }) => (
+        <Icon name="star" size={size} color={color} />
+      ),
+    }}
+  />
+</Tab.Navigator>
+);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={HomeTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
